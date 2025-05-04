7a511d9ace4701cd59899f2956320d5022d6771d
Daniel Speight <danielspeight4@gmail.com>
2025-05-03 22:06:31 -0500

    Added foundation of backpacks table

--- frontend/src/Components/Backpack.jsx
+++ frontend/src/Components/Backpack.jsx
@@ -1,7 +1,64 @@
+import { useEffect, useState } from "react";
+
 export default function Backpack() {
+  const [loading, setLoading] = useState(true);
+  const [backpacks, setBackpacks] = useState([{}]);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    fetchBackpacksByAdminRole();
+  }, []);
+
+  async function fetchBackpacksByAdminRole() {
+    setLoading(true);
+    const token = localStorage.getItem("token");
+    try {
+      const response = await fetch("/api/backpacks", {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      if (!response.ok) {
+        throw new Error(`HTTP error!  Status: ${response.status}`);
+      }
+      const data = await response.json();
+      console.log(data[0]);
+      setBackpacks(data);
+    } catch (error) {
+      setError(error);
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  if (error) {
+    return <div>{error}</div>;
+  }
+
   return (
     <div>
-      <p>Displaying the contents of the backpacks</p>
+      <table>
+        <tr>
+          <th>BackpackId</th>
+          <th>Name of Backpack</th>
+          <th>Location</th>
+          <th>User</th>
+          <th>Private?</th>
+        </tr>
+        {backpacks.map((val, key) => {
+          return (
+            <tr key={key}>
+              <td>{val.backpackId}</td>
+              <td>{val.backpackName}</td>
+              <td>{val.location}</td>
+              <td>{val.ownerUsername}</td>
+              <td>{val.privateValue ? "true" : "false"}</td>
+            </tr>
+          );
+        })}
+      </table>
     </div>
   );
 }
